1a5d54a2ce6a356f74c957548d46b3e668d8a78d
macro <makermuyi@163.com>
2021-07-07 20:29:06 +0800

    add encode8-3

---
+++ 74ls148/encode83a.v
@@ -0,0 +1,13 @@
+module encode83a (
+    input wire[7:0] x, 
+    output wire[2:0] y,
+    output wire vaild
+);
+
+assign y[2] = x[7] | x[6] | x[5] | x[4];  
+assign y[1] = x[7] | x[6] | x[3] | x[2];  
+assign y[0] = x[7] | x[5] | x[3] | x[1];  
+
+assign vaild = x[0] & ~y[2] & ~y[1] & ~y[0];
+    
+endmodule
