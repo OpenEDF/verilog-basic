3ffea7550fe8c1c73f2a70e17434475aaee6c175
macro <makermuyi@163.com>
2021-09-09 23:45:32 +0800

    add verilog operator

--- verilog-operators/arithmetic_operators.v
+++ verilog-operators/arithmetic_operators.v
@@ -3,7 +3,16 @@
 module arithmetic_operators();
 
 initial begin
-    $display("5 + 10 = %d", 5 + 10);
+    $display("5  + 10 = %d", 5 + 10);
+    $display("5  - 10 = %d", 5 - 10);
+    $display("10 - 5  = %d", 10 - 5);
+    $display("5  * 10 = %d", 5 * 10);
+    $display("10 / 5  = %d", 10 / 5);
+    $display("10 / -2 = %d", 10 / -2);
+    $display("10 %s 3 = %d", "%", 10 % 3);
+    $display("+5      = %d", +5);
+    $display("-5      = %d", -5);
+
     $finish;
 end
 
