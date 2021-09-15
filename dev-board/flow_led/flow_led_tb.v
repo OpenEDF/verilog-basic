4cab0beb71a2fce6a301e070963fad017ee5d3c6
macro <makermuyi@163.com>
2021-09-15 23:55:34 +0800

    add flow led sch picture

---
+++ dev-board/flow_led/flow_led_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+
+module flow_led_tb();
+
+parameter T =20;
+
+/* define register */
+reg sys_clk;
+reg sys_rst_n;
+
+/* define led */
+wire [7:0] led;
+
+/* initalization signal */
+initial begin
+    sys_clk          = 1'b0;
+    sys_rst_n        = 1'b0;
+    #(T+1) sys_rst_n = 1'b1;
+end
+
+/* generater clock */
+always #(T/2) sys_clk = ~sys_clk;
+
+flow_led u0_flow_led (
+    .sys_clk(sys_clk),
+    .sys_rst_n(sys_rst_n),
+    .led_out(led)
+);
+
+endmodule
