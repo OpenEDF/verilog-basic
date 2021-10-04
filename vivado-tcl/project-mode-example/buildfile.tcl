1c7192e7e2631cb0a30d325e72d8702a3797d5db
macro <makermuyi@163.com>
2021-10-04 09:22:15 +0800

    add the memory cfg tcl script

--- vivado-tcl/project-mode-example/buildfile.tcl
+++ vivado-tcl/project-mode-example/buildfile.tcl
@@ -1,77 +1,57 @@
 ###-----------------------------------------------------------------
-### vivado tcl file for Project mode
+### vivado tcl Create a configuration file to program the device
 ### Author: Macro
-### Resources: Vivado Design Suite User Guide Design Flows Overview
+### Reference: Vivado Design Suite User Guide Using Tcl Scripting(ug894)
 ### 
 
-puts "-------------------------- Start --------------------------"
-# Set sources file
-set verilog_src [list]
-lappend verilog_src ./sources/flowled.v
+# 1. set the bit file and flash mcs file path, accroding the project setup
+set cfgmem_path ./build
+file mkdir $cfgmem_path
+set bitfile_path  ./flowled.runs/impl_1/flowled.bit
+set flashmcs_path $cfgmem_path/borad_flash.mcs
 
-set sim_src ./sources/flowled_tb.v
-set constrs_src ./sources/flowled_xdc.xdc
-set output_dir ./output
-set bit_output_path ./flowled.runs/impl_1/flowled.bit
-
-# Set the jobs
-set pros_jobs 2
-
-# Set the FPGA chip
-set xilinx_fpga_chip xc7a35tfgg484-2
-set project_path [pwd]
-set project_name "flowled"
-
-# create project
-create_project -part $xilinx_fpga_chip $project_name $project_path 
-set_property DESIGN_MODE RTL [current_fileset]
-
-# Add design source file
-add_files -fileset sources_1 $verilog_src
-add_files -fileset sim_1 $sim_src
-add_files -fileset constrs_1 -norecurse $constrs_src
-update_compile_order -fileset sources_1
-update_compile_order -fileset sim_1
-
-# Launch Synthesis
-launch_runs -jobs $pros_jobs synth_1
-wait_on_run synth_1
-open_run synth_1 -name netlist_1
-
-# Generate a timing and power reports and write to disk
-file mkdir $output_dir
-report_timing_summary -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file $output_dir/syn_timing.rpt
-report_power -file $output_dir/syn_power.rpt
-
-# Launch Implementation
-launch_runs impl_1 -to_step write_bitstream
-wait_on_run impl_1 
-
-# comment out the open_run for batch mode
-open_run impl_1
-report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file $output_dir/imp_timing.rpt
-report_power -file $output_dir/imp_power.rpt
-
-# comment out the for batch mode
-# start_gui
-
-# connect device
+# Open and connect device
+#disconnect_hw_server localhost:3121
 open_hw_manager
-# disconnect_hw_server localhost:3121
 connect_hw_server -allow_non_jtag
 open_hw_target
-
-# add the path for programmed file
-set program_files [list]
-lappend program_files $bit_output_path
-
-set_property PROGRAM.FILE $program_files [get_hw_devices xc7a35t_0]
 current_hw_device [get_hw_devices xc7a35t_0]
 refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xc7a35t_0] 0]
 
-# write the bit file to device
-program_hw_devices [get_hw_devices xc7a35t_0]
+# Create a configuration file to program the device
+# Create a configuration file to program the device
+set cfgmem_argc [list up 0x00000000]
+lappend cfgmem_argc "$bitfile_path"
+write_cfgmem  -format mcs -size 128 -interface SPIx4 -loadbit $cfgmem_argc -checksum -force -disablebitswap -file "$flashmcs_path"
+
+# Add memory device to FPGA
+create_hw_cfgmem -hw_device [lindex [get_hw_devices xc7a35t_0] 0] [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0]
+set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
 refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0]
-close_hw_manager
+
+# Select a configuration file and set programming options.
+set flash_mscs [list]
+lappend flash_mscs $flashmcs_path
+set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.FILES $flash_mscs [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.PRM_FILE {} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+startgroup 
+create_hw_bitstream -hw_device [lindex [get_hw_devices xc7a35t_0] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices xc7a35t_0] 0]]; program_hw_devices [lindex [get_hw_devices xc7a35t_0] 0]; refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0];
+program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+endgroup
+
+# close the connect
+delete_hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [get_hw_devices xc7a35t_0 ]]
+close_hw_target
 
 ###-----------------------------------------------------------------
