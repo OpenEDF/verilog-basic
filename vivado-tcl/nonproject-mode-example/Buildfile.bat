61e753d1b6873b1f605db8b24936d3fc3b8dc803
macro <makermuyi@163.com>
2021-10-06 00:55:11 +0800

    add the non-project-mode batch script

--- vivado-tcl/nonproject-mode-example/Buildfile.bat
+++ vivado-tcl/nonproject-mode-example/Buildfile.bat
@@ -7,6 +7,7 @@
 set build=%1
 :: set the target
 set target=flowled
+set env_path=D:\Xilinx\Vivado\2019.2\settings64.bat
 echo ----------------------------------------------------
 echo The vaild command is build/flash/clean.
 
@@ -15,14 +16,14 @@ echo The vaild command is build/flash/clean.
   :: Call and mask out invalid call targets
   goto :%build% 2>nul || (
     :: Default case
-    echo The vaild command is build/flash/clean.
+    echo The vaild command is build/flash/clean/env.
   )
   goto :switch-case-end
   
   :build
 	:: build
     echo %build%
-	vivado -mode tcl -source project_mode_build.tcl
+	vivado -mode tcl -source nonproject_mode_build.tcl
     goto :switch-case-end     
   :flash
 	:: flash programming
@@ -32,8 +33,13 @@ echo The vaild command is build/flash/clean.
   :clean
 	:: clean the file and floder
     echo %build%
-	rmdir /s /q output build .Xil %target%.hw %target%.ip_user_files %target%.runs %target%.cache
-	del %target%.xpr vivado.* vivado_* webtalk.* webtalk_*
+	rmdir /s /q output build .Xil
+	del usage_statistics_webtalk.* hs_err_pid6440.* vivado.* vivado_* webtalk.*
+    goto :switch-case-end
+  :env
+	:: set vivado windows env path
+    echo %build%
+	%env_path%
     goto :switch-case-end
 :switch-case-end
    echo Command execution complete.
