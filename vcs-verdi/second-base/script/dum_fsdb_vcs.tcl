2db25eba920b9066b976be7e2ceb9bc0c732c5a6
macro <makermuyi@163.com>
2021-10-11 08:46:13 +0800

    add the vcs verdi example

---
+++ vcs-verdi/second-base/script/dum_fsdb_vcs.tcl
@@ -0,0 +1,4 @@
+global env
+fsdbDumpfile "$env(OUTPUT).fsdb"
+fsdbDumpvars 0 "flow_led_tb"
+run
---
+++ vcs-verdi/second-base/tb/flowled_tb.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+
+module flow_led_tb();
+
+parameter T =20;
+
+/* define register */
+reg sys_clk;
+reg sys_rst_n;
+
+/* define led */
+wire [7:0] led;
+
+initial begin
+    $display("Simluation Start!\n");
+end      
+
+/* initalization signal */
+initial begin
+    sys_clk          = 1'b0;
+    sys_rst_n        = 1'b0;
+    #(T+1) sys_rst_n = 1'b1;
+end
+
+/* generater clock */
+always #(T/2) sys_clk = ~sys_clk;
+
+flow_led u0_flow_led (
+    .sys_clk(sys_clk),
+    .sys_rst_n(sys_rst_n),
+    .led_out(led)
+);
+
+/* dump dvp file */
+//initial begin
+  //  $cvdpluson;
+//end
+
+//initial #10000 $stop;
+
+initial begin
+    #2000
+    $display("Simluation End!\n");
+    $finish;
+end
+
+endmodule
