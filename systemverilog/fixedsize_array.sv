5b67772142ec25e0b87d468588c9635d4c244a8a
macro <makermuyi@163.com>
2021-10-18 16:46:39 +0800

    add the systemverilog

--- systemverilog/fixedsize_array.sv
+++ systemverilog/fixedsize_array.sv
@@ -1,28 +1,15 @@
 module fixedsize_array;
   //declaration of array’s
-  int array1[6];               //single dimension array
-  int array2[5:0];             //single dimension array
-  int array3[2:0][3:0];        //multi dimension array
-  int array4[4:0];
- 
+  int array[2];               //single dimension array
+
 initial begin
     //array initialization
-    array1 = '{0,1,2,3,4,5};
-    array2 = '{0,1,2,3,4,5};
-    array3 = '{'{0,1,2,3},'{4,5,6,7},'{8,9,10,11}};
+    array[0] = 258;
+    array[1] = 369;
  
     //displaying array elements
-    $display("-------displaying array1-------");
-    foreach(array1[i]) $display("\t array1[%0d] = %0d",i,array1[i]);
- 
-    $display("-------displaying array2-------");
-    for(int i=0;i<6;i++) $display("\t array2[%0d] = %0d",i,array2[i]);
- 
-    $display("-------displaying array3-------");
-    foreach(array3[i,j]) $display("\t array3[%0d][%0d] = %0d",i,j,array3[i][j]);
- 
-    $display("-------displaying uninitialized array4-------");
-    for(int i=0;i<5;i++) $display("\t array4[%0d] = %0d",i,array4[i]);
+    $display("-------displaying array-------");
+    foreach(array[i]) $display("\t array1[%0d] = %0d",i,array[i]);
 end
 
 endmodule
