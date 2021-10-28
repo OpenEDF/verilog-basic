e70066af2902e313db0137fd2ea515f5455df74b
macro <makermuyi@163.com>
2021-10-28 14:21:34 +0800

    add the verilog code style

---
+++ verilog_code_style.v
@@ -0,0 +1,19 @@
+// # 1. Verilog File Header:
+
+
+// # 2. Verilog Code style:
+module [module_name] (
+    [mode] [data_type] [port_names],
+    [mode] [data_type] [port_names],
+    [mode] [data_type] [port_names]
+);
+
+
+
+endmodule
+
+// #3. File comment
+
+//--------------------------------------------------------------------------
+//
+//--------------------------------------------------------------------------
