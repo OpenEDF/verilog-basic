560b21df61831f2cd10779720160197b077edd10
macro <makermuyi@163.com>
2022-03-17 23:07:44 +0800

    Fix cs_server is not close

--- vivado-tcl/create-conf-progran-device/buildfile.tcl
+++ vivado-tcl/create-conf-progran-device/buildfile.tcl
@@ -10,50 +10,54 @@ file mkdir $cfgmem_path
 set bitfile_path  ./flowled.runs/impl_1/flowled.bit
 set flashmcs_path $cfgmem_path/borad_flash.mcs
 
-# Open and connect device
-#disconnect_hw_server localhost:3121
+# target device info
+set hw_fpga "xc7a35t_0"
+
+# 2. Open and connect device
+disconnect_hw_server localhost:3121
 open_hw_manager
 connect_hw_server -allow_non_jtag
 open_hw_target
-current_hw_device [get_hw_devices xc7a35t_0]
-refresh_hw_device -update_hw_probes false [lindex [get_hw_devices xc7a35t_0] 0]
+current_hw_device [get_hw_devices $hw_fpga]
+refresh_hw_device -update_hw_probes false [lindex [get_hw_devices $hw_fpga] 0]
 
-# Create a configuration file to program the device
-# Create a configuration file to program the device
+#3. Create a configuration file to program the device
 set cfgmem_argc [list up 0x00000000]
 lappend cfgmem_argc "$bitfile_path"
 write_cfgmem  -format mcs -size 128 -interface SPIx4 -loadbit $cfgmem_argc -checksum -force -disablebitswap -file "$flashmcs_path"
 
-# Add memory device to FPGA
-create_hw_cfgmem -hw_device [lindex [get_hw_devices xc7a35t_0] 0] [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0]
-set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0]
+#4. Add memory device to FPGA
+create_hw_cfgmem -hw_device [lindex [get_hw_devices $hw_fpga] 0] [lindex [get_cfgmem_parts {mt25ql128-spi-x1_x2_x4}] 0]
+set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+refresh_hw_device [lindex [get_hw_devices $hw_fpga] 0]
 
-# Select a configuration file and set programming options.
+#5. Select a configuration file and set programming options.
 set flash_mscs [list]
 lappend flash_mscs $flashmcs_path
-set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.FILES $flash_mscs [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.PRM_FILE {} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
-set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.FILES $flash_mscs [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.PRM_FILE {} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
+set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
 startgroup 
-create_hw_bitstream -hw_device [lindex [get_hw_devices xc7a35t_0] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices xc7a35t_0] 0]]; program_hw_devices [lindex [get_hw_devices xc7a35t_0] 0]; refresh_hw_device [lindex [get_hw_devices xc7a35t_0] 0];
-program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices xc7a35t_0] 0]]
+create_hw_bitstream -hw_device [lindex [get_hw_devices $hw_fpga] 0] [get_property PROGRAM.HW_CFGMEM_BITFILE [ lindex [get_hw_devices $hw_fpga] 0]]; program_hw_devices [lindex [get_hw_devices $hw_fpga] 0]; refresh_hw_device [lindex [get_hw_devices $hw_fpga] 0];
+program_hw_cfgmem -hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices $hw_fpga] 0]]
 endgroup
 
-# close the connect
-delete_hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [get_hw_devices xc7a35t_0 ]]
+#6. close the connect
+delete_hw_cfgmem [ get_property PROGRAM.HW_CFGMEM [get_hw_devices $hw_fpga ]]
 close_hw_target
+disconnect_hw_server
+close_hw_manager
 
-# quit the tcl
+#7. quit the tcl
 quit
 ###-----------------------------------------------------------------
