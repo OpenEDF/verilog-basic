45f061abc3aa1ae96e66b5fbb3e55d7cfe2fb890
macro <makermuyi@163.com>
2023-08-05 10:40:35 +0800

    add the xilinx fpga prj

---
+++ identify_example/xilinx/idc_vivado/idc_vivado.runs/impl_1/runme.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# 
+# Vivado(TM)
+# runme.sh: a Vivado-generated Runs Script for UNIX
+# Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+# 
+
+if [ -z "$PATH" ]; then
+  PATH=/home/macro/xilinx/Vitis/2019.2/bin:/home/macro/xilinx/Vivado/2019.2/ids_lite/ISE/bin/lin64:/home/macro/xilinx/Vivado/2019.2/bin
+else
+  PATH=/home/macro/xilinx/Vitis/2019.2/bin:/home/macro/xilinx/Vivado/2019.2/ids_lite/ISE/bin/lin64:/home/macro/xilinx/Vivado/2019.2/bin:$PATH
+fi
+export PATH
+
+if [ -z "$LD_LIBRARY_PATH" ]; then
+  LD_LIBRARY_PATH=
+else
+  LD_LIBRARY_PATH=:$LD_LIBRARY_PATH
+fi
+export LD_LIBRARY_PATH
+
+HD_PWD='/home/macro/github/verilog-basic/identify_example/xilinx/idc_vivado/idc_vivado.runs/impl_1'
+cd "$HD_PWD"
+
+HD_LOG=runme.log
+/bin/touch $HD_LOG
+
+ISEStep="./ISEWrap.sh"
+EAStep()
+{
+     $ISEStep $HD_LOG "$@" >> $HD_LOG 2>&1
+     if [ $? -ne 0 ]
+     then
+         exit
+     fi
+}
+
+# pre-commands:
+/bin/touch .write_bitstream.begin.rst
+EAStep vivado -log led_stream.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source led_stream.tcl -notrace
+
+
