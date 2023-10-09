9cdd8f8ba73376bdaf0ffd9b297b2ec092afc6fd
macro <makermuyi@163.com>
2023-10-09 23:57:42 +0800

    add template for test learn

---
+++ template/testbench.v
@@ -0,0 +1,78 @@
+//--------------------------------------------------------------------------
+//                         RISC-V Core
+//                            V1.0.0
+//                         openedf.com
+//                     Copyright 2023-2024
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: macro
+// Brief:
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Module
+//--------------------------------------------------------------------------
+module testbench();
+//--------------------------------------------------------------------------
+// Ports
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+// Design: create the clock
+//--------------------------------------------------------------------------
+initial begin
+    clk = 1'b0;
+    forever #5 clk = ~clk;
+end
+
+//--------------------------------------------------------------------------
+// Design: initial
+//--------------------------------------------------------------------------
+initial begin
+    rst_n = 1'b0;
+    #10
+    rst_n = 1'b1;
+
+    #20
+    $finish();
+end
+
+//--------------------------------------------------------------------------
+// Design: dump .fsdb file
+//--------------------------------------------------------------------------
+initial begin
+    $fsdbDumpfile("testbench.fsdb");
+    $fsdbDumpvars(0, testbench);
+end
+
+//--------------------------------------------------------------------------
+// Design: instance design module
+//--------------------------------------------------------------------------
+
+endmodule
+//--------------------------------------------------------------------------
