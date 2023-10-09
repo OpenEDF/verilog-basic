c1b8582d968be68ca01e51637ed7fd7c94006696
macro <makermuyi@163.com>
2023-10-09 22:51:29 +0800

    example: uvm phase

--- uvm/uvm_phase/new_world.sv
+++ uvm/uvm_phase/new_world.sv
@@ -17,4 +17,15 @@ class new_world extends uvm_test;
     virtual function void build_phase(uvm_phase phase);
         `uvm_info(tid, "build phase is executed !!!", UVM_LOW);
     endfunction
+
+    /* connect phase function */
+    virtual function void connect_phase(uvm_phase phase);
+        `uvm_info(tid, "connect phase is executed !!!", UVM_LOW);
+    endfunction
+
+    /* uvm uvm_end_of_elaboration_phase */
+    virtual function void uvm_end_of_elaboration_phase(uvm_phase phase);
+        `uvm_info(tid, "uvm_end_of_elaboration_phase phase is executed !!!", UVM_LOW);
+    endfunction
+
 endclass: new_world
