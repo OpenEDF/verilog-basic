5e0cbf74ae6ce19d2be04b9c765061cd908d4bd9
macro <makermuyi@163.com>
2023-10-16 08:49:51 +0800

    add oops example init

---
+++ sverilog_oops/oops_basic/test_intf.sv
@@ -0,0 +1,74 @@
+//--------------------------------------------------------------------------
+//                         RISC-V Core
+//                            V1.0.0
+//                         openedf.com
+//                     Copyright 2023-2024
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: macro
+// Brief: a practical guide for systemverilog asserations example and learning
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Interface
+//--------------------------------------------------------------------------
+interface test_intf(input logic clk, rst_n);
+//--------------------------------------------------------------------------
+// Port
+//--------------------------------------------------------------------------
+logic [4:0] a_in;
+logic [4:0] b_in;
+logic [4:0] c_ou;
+logic [4:0] d_ou;
+
+//--------------------------------------------------------------------------
+// modport: define modport for dut
+//--------------------------------------------------------------------------
+modport dut_intf(
+    input  clk,
+    input  rst_n,
+    input  a_in,
+    input  b_in,
+
+    output c_ou,
+    output d_ou
+);
+
+//--------------------------------------------------------------------------
+// modport: define modport for coverage group
+//--------------------------------------------------------------------------
+modport cover_intf(
+    input  clk,
+
+    input  c_ou,
+    input  d_ou
+);
+
+endinterface
+//--------------------------------------------------------------------------
---
+++ sverilog_oops/oops_basic/testbench.sv
@@ -0,0 +1,120 @@
+//--------------------------------------------------------------------------
+//                         RISC-V Core
+//                            V1.0.0
+//                         openedf.com
+//                     Copyright 2023-2024
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: macro
+// Brief: testbenchi systemverilog assertions
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Module
+//--------------------------------------------------------------------------
+module testbench();
+//--------------------------------------------------------------------------
+// Ports
+//--------------------------------------------------------------------------
+bit clk;
+bit rst_n;
+
+//--------------------------------------------------------------------------
+// Interface
+//--------------------------------------------------------------------------
+test_intf intf(clk, rst_n);
+
+//--------------------------------------------------------------------------
+// Design: create the clock
+//--------------------------------------------------------------------------
+initial begin
+    clk = 1'b0;
+    forever #5 clk = ~clk;
+end
+
+//--------------------------------------------------------------------------
+// Design: driver task and initial
+//--------------------------------------------------------------------------
+task driver();
+begin
+    rst_n = 1'b0;
+    #10
+    rst_n = 1'b1;
+
+    repeat(200) begin
+        @(posedge clk)
+        intf.a_in =$random;
+        intf.b_in =$random;
+    end
+    #20
+    $display("Coverage = %0.2f %%", fun_cover.cov1_u.get_coverage());
+    $finish();
+end
+endtask
+
+// start driver
+initial begin
+    driver();
+end
+
+//--------------------------------------------------------------------------
+// Design: dump .fsdb file
+//--------------------------------------------------------------------------
+initial begin
+    $fsdbDumpfile("testbench.fsdb");
+    $fsdbDumpvars(0, testbench);
+end
+
+//--------------------------------------------------------------------------
+// Design: instance function coverage module
+//--------------------------------------------------------------------------
+fun_cover fun_cover_u (
+    .clk_cov         (clk),
+    
+    .c_ou_cov        (intf.c_ou),
+    .d_ou_cov        (intf.d_ou)
+);
+
+//--------------------------------------------------------------------------
+// Design: instance design module
+//--------------------------------------------------------------------------
+cover_dut cover_dut_u (
+    // inputs
+    .clk         (clk),
+    .rst_n       (rst_n),
+    .a_in        (intf.a_in),
+    .b_in        (intf.b_in),
+
+    // outputs
+    .c_ou        (intf.c_ou),
+    .d_ou        (intf.d_ou)
+);
+
+endmodule
+//--------------------------------------------------------------------------
