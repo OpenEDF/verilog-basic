c8b33e8331bc2dc7e191e01d37787e71ad44dd0a
macro <makermuyi@163.com>
2023-10-22 10:15:29 +0800

    add port port export export imp port

---
+++ tlm/port_port_export_export_imp/component_b_a_a.sv
@@ -0,0 +1,66 @@
+//--------------------------------------------------------------------------
+//                            UVM Lab
+//                         openedf.com
+//                     Copyright 2023-2024
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: macro
+// Brief: connecting tlm port and imp port
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Class
+//--------------------------------------------------------------------------
+class component_b_a_a extends uvm_component;
+
+//--------------------------------------------------------------------------
+// Design: declaring non-blocking iput imp port
+//--------------------------------------------------------------------------
+transaction trans;
+uvm_blocking_put_imp #(transaction, component_b_a_a) trans_in;
+`uvm_component_utils(component_b_a_a)
+
+//--------------------------------------------------------------------------
+// Design: component_b new method
+//--------------------------------------------------------------------------
+function new(string name, uvm_component parent);
+    super.new(name, parent);
+    trans_in = new("trans_in", this);
+endfunction: new
+
+//--------------------------------------------------------------------------
+// Design: imp the try put method
+//--------------------------------------------------------------------------
+virtual function bit put(transaction trans);
+    `uvm_info(get_type_name(), $sformatf("Recived trans on sub --> sub blocking imp port"), UVM_LOW)
+    `uvm_info(get_type_name(), $sformatf("printing the trans, \n %s", trans.sprint()), UVM_LOW)
+endfunction: put
+
+endclass: component_b_a_a
+//--------------------------------------------------------------------------
