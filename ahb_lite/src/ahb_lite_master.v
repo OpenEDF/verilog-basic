75f2e78e8d83339618cf7e5baf802dde3b9c445e
macro <makeryang@163.com>
2024-01-05 21:08:24 +0800

    update the Makefile support centos compiler

--- ahb_lite/src/ahb_lite_master.v
+++ ahb_lite/src/ahb_lite_master.v
@@ -47,30 +47,109 @@ module ahb_lite_master
     input wire         HCLK,
     input wire         HRESETn,
 
-    // AHB master input
+    // AHB master inputs
+    input wire [31:0]  master_in_wdata,
+    input wire [31:0]  master_in_addr,
+    input wire         master_en,
+    input wire         master_wr,
     input wire         HREADY,
     input wire         HRESP,
     input wire [31:0]  HRDATA,
 
     // AHB master outputs
-    output reg  [31:0] HADDR,
+    output reg [31:0]  HADDR,
     output reg         HWRITE,
-    output reg  [2:0]  HSIZE.
-    output reg  [2:0]  HBURST,
-    output reg  [1:0]  HTRANS,
+    output reg [2:0]   HSIZE.
+    output reg [2:0]   HBURST,
+    output reg [1:0]   HTRANS,
     output reg         HMASTERLOCK,
-    output reg  [31:0] HWDATA,
+    output reg [31:0]  HWDATA,
 
-    //custom
-    output reg         BUSY
+    //custom master outputs
+    output reg         master_hreadyn_wait,
+    output reg         master_out_data
 );
 
 //--------------------------------------------------------------------------
-// Design: master FSM
+// Design: module internal signals
 //--------------------------------------------------------------------------
+reg [31:0] master_address_phase_data;
+`define DEF_ACCRESS_SLAVE_ADDR 32'hF000_0000
+localparam MASTER_IDLE = 4'b01;
+localparam MASTER_WORK = 4'b01;
 
 //--------------------------------------------------------------------------
-// Design: output address and control signal
+// Design: master FSM control address phase and data phase impletement the
+//         pipeline transaction
+//--------------------------------------------------------------------------
+always @(posedge HCLK or negedge HRESETn) begin
+    if (!HRESETn) begin
+        master_cur_state <= MASTER_IDLE;
+    end else begin
+        master_cur_state <= master_next_state;
+    end
+
+//--------------------------------------------------------------------------
+// Design: master FSM  update
+//--------------------------------------------------------------------------
+always @(*) begin
+    if (!HRESETn) begin
+        master_next_state <= MASTER_IDLE;
+    end else begin
+        case (master_cur_state)
+            MASTER_IDLE:
+                if (master_en) begin
+                    master_next_state <= MASTER_WORK;
+                end else begin
+                    master_next_state <= MASTER_IDLE;
+                end
+            MASTER_WORK:
+                if (!master_en) begin
+                    master_next_state <= MASTER_IDLE;
+                end else begin
+                    master_next_state <= MASTER_WORK;
+                end
+            default:
+                master_next_state <= MASTER_IDLE;
+        endcase
+    end
+end
+
+//--------------------------------------------------------------------------
+// Design: address phase, master output the address, _data and control 
+//         signal 
+//--------------------------------------------------------------------------
+always @(posedge HCLK or negedge HRESETn) begin
+    if (!HRESETn) begin
+        HADDR       <= `DEF_ACCRESS_SLAVE_ADDR;
+        HWRITE      <= 1'b0;
+        HSIZE       <= 3'b010;
+        HBRUST      <= 3'b000;
+        HTRANS      <= 2'b00;
+        HMASTERLOCK <= 1'b0; 
+        HWDATA      <= 32'h0000_0000;
+        master_address_phase_data <= 32'h0000_0000;
+    end else begin
+        case (master_cur_state)
+            master_address_phase_data <= master_in_wdata;
+
+        endcase
+    end
+end
+
+//--------------------------------------------------------------------------
+// Design: data phase, output the data from master address phase
+//--------------------------------------------------------------------------
+always @(posedge HCLK or negedge HRESETn) begin
+    if (!HRESETn) begin
+        HWDATA <= 32'h0000_0000;
+    end else begin
+        HWDATA <= master_address_phase_data;
+    end
+end
+
+//--------------------------------------------------------------------------
+// Design: read response data and control singal 
 //--------------------------------------------------------------------------
 
 endmodule
