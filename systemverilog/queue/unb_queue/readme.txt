2bb6c749414c65447710c7544b056976c80ea7cd
Macro <makermuyi@gmail.com>
2024-07-11 23:59:34 +0800

    add example that systemverilog queue

---
+++ systemverilog/queue/unb_queue/readme.txt
@@ -0,0 +1,25 @@
+Chronologic VCS simulator copyright 1991-2018
+Contains Synopsys proprietary information.
+Compiler version O-2018.09-SP2_Full64; Runtime version O-2018.09-SP2_Full64;  Jul 11 23:55 2024
+-------- queue 1 size is 4 --------
+	queue_1[0] = 0
+	queue_1[1] = 1
+	queue_1[2] = 2
+	queue_1[3] = 3
+-------- queue 1 size after inserting is 5 --------
+	queue_1[0] = 0
+	queue_1[1] = 6
+	queue_1[2] = 1
+	queue_1[3] = 2
+	queue_1[4] = 3
+-------- queue 1 size after delete is 4 --------
+	queue_1[0] = 0
+	queue_1[1] = 6
+	queue_1[2] = 1
+	queue_1[3] = 2
+$finish at simulation time                    0
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0
+CPU Time:      0.430 seconds;       Data structure size:   0.0Mb
+Thu Jul 11 23:56:01 2024
+CPU time: .189 seconds to compile + .212 seconds to elab + .247 seconds to link + .455 seconds in simulation
---
+++ systemverilog/queue/unb_queue/testbench.sv
@@ -0,0 +1,76 @@
+//--------------------------------------------------------------------------
+//                      systemverilog calss
+//                            V1.0.0
+//                         openedf.com
+//                     Copyright 2023-2024
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: macro
+// Brief: systemverilog class test
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Module
+//--------------------------------------------------------------------------
+program tb_top;
+
+//--------------------------------------------------------------------------
+// Design: declaration data type
+//--------------------------------------------------------------------------
+bit    [31:0] queue_1[$];
+string        queue_2[$];
+
+//--------------------------------------------------------------------------
+// Design: inital
+//--------------------------------------------------------------------------
+initial begin
+    /* queue initialization */
+    queue_1 = {0, 1, 2 ,3};
+    queue_2 = {"red", "blue", "green"};
+
+    /* size-method */
+    $display("-------- queue 1 size is %0d --------", queue_1.size());
+    foreach(queue_1[i])
+        $display("\tqueue_1[%0d] = %0d", i, queue_1[i]);
+
+    /* insert method */
+    queue_1.insert(1, 6);
+    $display("-------- queue 1 size after inserting is %0d --------", queue_1.size());
+    foreach(queue_1[i])
+        $display("\tqueue_1[%0d] = %0d", i, queue_1[i]);
+
+    /* delete method */
+    queue_1.delete(4);
+    $display("-------- queue 1 size after delete is %0d --------", queue_1.size());
+    foreach(queue_1[i])
+        $display("\tqueue_1[%0d] = %0d", i, queue_1[i]);
+end
+
+endprogram
+//--------------------------------------------------------------------------
