c1fbabdbf57337fa6105e0e87db3f14469c12319
Macro <makermuyi@gmail.com>
2024-08-01 23:42:30 +0800

    add class virtual sample

---
+++ systemverilog/virtual/class_v/readme.txt
@@ -0,0 +1,15 @@
+Chronologic VCS simulator copyright 1991-2018
+Contains Synopsys proprietary information.
+Compiler version O-2018.09-SP2_Full64; Runtime version O-2018.09-SP2_Full64;  Aug  1 23:40 2024
+[0] inside base class
+[0] inside sum function
+[0] inside extended class
+[0] data = 0x567800ff
+[0] addr = 0xff0000ff
+[0] status = 0x1
+[0] sum = 0x56780100
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0
+CPU Time:      0.360 seconds;       Data structure size:   0.0Mb
+Thu Aug  1 23:40:19 2024
+CPU time: .276 seconds to compile + .284 seconds to elab + .231 seconds to link + .390 seconds in simulation
---
+++ systemverilog/virtual/class_v/testbench.sv
@@ -0,0 +1,93 @@
+//--------------------------------------------------------------------------
+//                      systemverilog calss
+//                            V1.0.0
+//                         openedf.com
+//                     Copyright 2023-2024
+//
+//                     makermuyi@gmail.com
+//
+//                       License: BSD
+//--------------------------------------------------------------------------
+//
+// Copyright (c) 2020-2021, openedf.com
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+//--------------------------------------------------------------------------
+// Designer: macro
+// Brief: systemverilog class test
+// Change Log:
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Include File
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+// Class 
+//--------------------------------------------------------------------------
+virtual class base_class;
+
+int data;
+int addr;
+int status;
+int sum;
+    
+function new();
+    data = 32'h1234_abcd;
+    $display("[%0t] inside base class", $time);
+endfunction
+
+virtual function calsulate_sum(int a, int b);
+    this.sum = a + b; 
+    $display("[%0t] inside sum function", $time);
+endfunction
+
+endclass
+
+class extended_class extends base_class;
+    
+function new();
+    this.data = 32'h5678_00FF;
+    this.addr = 32'hFF00_00FF;
+    this.status = 32'h0000_0001;
+    this.calsulate_sum(this.data, this.status);
+    $display("[%0t] inside extended class", $time);
+endfunction
+
+endclass
+
+//--------------------------------------------------------------------------
+// Module
+//--------------------------------------------------------------------------
+module tb_top;
+
+//--------------------------------------------------------------------------
+// Design: declaration data type
+//--------------------------------------------------------------------------
+extended_class child;
+//--------------------------------------------------------------------------
+// Design: inital
+//--------------------------------------------------------------------------
+initial begin
+    child = new();
+    $display("[%0t] data = 0x%0h", $time, child.data);
+    $display("[%0t] addr = 0x%0h", $time, child.addr);
+    $display("[%0t] status = 0x%0h", $time, child.status);
+    $display("[%0t] sum = 0x%0h", $time, child.sum);
+end
+
+endmodule
+//--------------------------------------------------------------------------
